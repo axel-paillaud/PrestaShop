256dee9ae1a8eca63af1b6bf32686ce3b5a58994
Axel Paillaud <contact@axelweb.fr>
2025-02-27 20:13:40 +0100

    update

--- modules/productcomments/views/js/list-comments.js
+++ modules/productcomments/views/js/list-comments.js
@@ -65,7 +65,7 @@ jQuery(document).ready(function () {
     reportCommentPostErrorModal.modal('show');
   }
 
-  async function fetchComments(page) {
+  async function fetchComments(page) {  
     let response = await fetch(commentsListUrl + "&page=" + page);
 
     if (response.status === 200) {
@@ -73,12 +73,12 @@ jQuery(document).ready(function () {
         populateComments((JSON.parse(data)).comments);
     }
   }
-
+  
   $(pagesListId + ' li').on('click',
     function() {
       let oldCount = commentsList.data('current-page');
       let newCount = $(this).index();
-
+      
       if (newCount === prevCount) {  // click prev
         newCount = oldCount - 1;
         if (newCount <= 0) return;
@@ -87,21 +87,21 @@ jQuery(document).ready(function () {
         newCount = oldCount + 1;
         if (newCount >= nextCount) return;
       }
-
+      
       $(`${pageIdPrefix}${oldCount} span`).removeClass('current');
       $(`${pageIdPrefix}${oldCount}`).removeClass('active');
-
-      fetchComments(newCount); // fetch new page's comments
+      
+      fetchComments(newCount); // fetch new page's comments                  
 
       $(`${pageIdPrefix}${newCount}`).addClass('active');
       $(`${pageIdPrefix}${newCount} span`).addClass('current');
-
+      
       $(`${pageIdPrefix}${newCount} span`).html(newCount);
-      commentsList.data('current-page', newCount);
+      commentsList.data('current-page', newCount);      
 
       if (newCount === 1) // disable prev
         $(`${pageIdPrefix}${prevCount}`).addClass('disabled');
-      else
+      else   
         $(`${pageIdPrefix}${prevCount}`).removeClass('disabled');
 
       if (newCount === totalPages)  // disable next
@@ -109,8 +109,8 @@ jQuery(document).ready(function () {
       else
         $(`${pageIdPrefix}${nextCount}`).removeClass('disabled');
 
-      // long list with over 9 pages
-      if (9 <= totalPages) {
+      // long list with over 9 pages      
+      if (9 <= totalPages) {        
         generateGap(newCount, prevCount);
         generateGap(newCount, nextCount);
       }
@@ -121,7 +121,7 @@ jQuery(document).ready(function () {
     if (start == stop)
       return 0;
     let step = (start < stop) ? +1 : -1;
-    let i = start + step;
+    let i = start + step;  
     if (4 < Math.abs(stop - start)) {
       $(`${pageIdPrefix}${i}`).removeClass('hidden').removeClass('disabled');
       $(`${pageIdPrefix}${i} span`).html(i);
@@ -131,14 +131,14 @@ jQuery(document).ready(function () {
       i = i + step;
       for (; i != stop - 2*step; i = i + step) {
         $(`${pageIdPrefix}${i}`).addClass('hidden');
-      }
+      }  
     }
     else {
-      for (; i != stop; i = i + step) {
+      for (; i != stop; i = i + step) {        
         $(`${pageIdPrefix}${i}`).removeClass('hidden').removeClass('disabled');
-        $(`${pageIdPrefix}${i} span`).html(i);
-      }
-    }
+        $(`${pageIdPrefix}${i} span`).html(i);        
+      }          
+    } 
   }
 
   function populateComments(comments) {
@@ -190,7 +190,7 @@ jQuery(document).ready(function () {
         body: "id_product_comment=" + commentId + "&usefulness=" + usefulness,
       });
 
-      if (response.status === 200) {
+      if (response.status === 200) {        
         const jsonData = await response.json();
         if (jsonData.success) {
           $('.useful-review-value', $comment).html(jsonData.usefulness);
@@ -223,11 +223,11 @@ jQuery(document).ready(function () {
         method: "POST",
         headers: {
           "Content-Type": "application/x-www-form-urlencoded",
-        },
+        },        
         body: "id_product_comment=" + commentId,
       });
 
-      if (response.status === 200) {
+      if (response.status === 200) {        
         const jsonData = await response.json();
         if (jsonData.success) {
           reportCommentPostedModal.modal('show');
@@ -244,9 +244,9 @@ jQuery(document).ready(function () {
 
   if (totalPages <= 1)
     $(pagesListId).hide();
-
+    
   if (totalPages > 0) {
     emptyProductComment.hide();
-    $(`${pageIdPrefix}1`).trigger('click');
-  }
+    $(`${pageIdPrefix}1`).trigger('click');    
+  }  
 });
